9b9532c493bb65c1548461bca3b87f1de86dc7c5
Naro <n.justinaro@gmail.com>
2022-02-10 16:29:15 -0500

    second draft. End of work day. score function is working

--- script.js
+++ script.js
@@ -9,41 +9,72 @@ function computerPlay(randNum){
     }
 }
 
+userScore = 0;
+opScore = 0;
+
 
 function playRound(computerSelection, userSelection) {
     if (computerSelection === userSelection){
         return "Tie"
     }else if (computerSelection === "rock") {
         if (userSelection === "paper") {
+            userScore +=1;
             return "You win";
         } else if (userSelection === "scissor") {
+            opScore += 1;
             return "You lose";
         }
     } else if (computerSelection === "paper") {
         if (userSelection === "scissor") {
+            userScore +=1;
             return "You win";
         } else if (userSelection === "rock") {
+            opScore += 1;
             return "You lose";
         }
     } else if (computerSelection === "scissor") {
         if (userSelection === "rock") {
+            userScore +=1;
             return "You win";
         } else if (userSelection ==="paper") {
+            opScore += 1;
             return "You Lose";
         }
     }
 }
 
+
 function playGame(){
-    for (let i = 0; i < 5; i ++){
-        let userChoice = prompt("R,P, or S?").toLowerCase();
-        let randomNumber = Math.floor(Math.random()*3) + 1;
-        let compChoice = computerPlay(randomNumber);
-        console.log(playRound(compChoice, userChoice))
-        if (i === 5){
-            console.log("Done")
-        }  
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    let compChoice = computerPlay(randomNumber);
+    console.log(playRound(compChoice, userChoice))
+    const scoreNum = document.querySelector('.scoreNum');
+
+    const userNum = document.getElementById("userNum");
+    userNum.textContent = userScore;
+    scoreNum.appendChild(userNum);
+
+    const opNum = document.getElementById('opNum');
+    opNum.textContent = opScore;
+    scoreNum.appendChild(opNum);        
     }
-}
 
 /*console.log(playGame())*/
+
+const rock = document.querySelector('#rock')
+rock.addEventListener('click', () => {
+    userChoice = "rock";
+    playGame();
+});
+
+const paper = document.querySelector('#paper')
+paper.addEventListener('click', () => {
+    userChoice = "paper";
+    playGame();
+});
+
+const scissor = document.querySelector('#scissor')
+scissor.addEventListener('click', () => {
+    userChoice = "scissor";
+    playGame();
+});
